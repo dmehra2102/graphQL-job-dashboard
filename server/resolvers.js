5f4cb8500dfd13fc1b324bdcde65c9858181bea6
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-04 00:06:13 +0530

    feat : fetching jobs list from database

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,15 @@
+import { getJobs } from "./db/jobs.js";
+
 export const resolvers = {
   Query: {
-    greeting: () => "Hello World!",
+    jobs: () => getJobs(),
+  },
+
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
   },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
