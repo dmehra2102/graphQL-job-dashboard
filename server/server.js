62af2a97f9f444237fde08ed806c3d00ec46c8cd
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-03 23:35:59 +0530

    feat : added graphql server with express server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
-import cors from 'cors';
-import express from 'express';
-import { authMiddleware, handleLogin } from './auth.js';
+import cors from "cors";
+import express from "express";
+import { resolvers } from "./resolvers.js";
+import { readFile } from "node:fs/promises";
+import { ApolloServer } from "@apollo/server";
+import { authMiddleware, handleLogin } from "./auth.js";
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
 
 const PORT = 9000;
 
 const app = express();
 app.use(cors(), express.json(), authMiddleware);
 
-app.post('/login', handleLogin);
+const typeDefs = await readFile("./schema.graphql", { encoding: "utf8" });
+
+app.post("/login", handleLogin);
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+
+app.use("/graphql", apolloMiddleware(apolloServer));
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Server url : http://localhost:${PORT}/graphql`);
 });
